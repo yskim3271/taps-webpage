bc53411ac7e381235d9124b9d590185d0547484e
Yunsik kim <hina3271@naver.com>
2025-04-13 13:42:47 +0900

    mod base path

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			precompress: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/taps-webpage' : '',
+			base: process.env.NODE_ENV === 'production' ? '' : '',
 			relative: false
 		},
 		prerender: {
